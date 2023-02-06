9e59ed76f6968ad9bede16628a0407924ee53a42
Me-Pankaj <118957086+Me-Pankaj@users.noreply.github.com>
2023-02-06 09:38:01 +0530

    Initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './footer.css'
+const Footer = () => {
+    return (
+        <div className="divi">
+           <a href="" target="_blank"> <button>Contact Me</button></a>
+        </div>
+    )
+}
+
+export default Footer
